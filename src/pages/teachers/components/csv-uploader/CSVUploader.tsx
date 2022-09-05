442ac62794c043d9b5866fa54f17a8dac83654a4
marcobaldi97 <60588169+marcobaldi97@users.noreply.github.com>
2022-09-05 17:05:32 -0300

    Squashed commit of the following:
    commit ba235753f2359d75d984a44e0d84bd5e45664963
    Author: Santiago <37115443+santiagosegundo96@users.noreply.github.com>
    Date:   Sat Sep 3 13:41:47 2022 -0300
    
        Create pr-web.yml
    
        * Create pr-web.yml
    
        * Adds package lock track
    
        Co-authored-by: marcobaldi97 <60588169+marcobaldi97@users.noreply.github.com>
    
    commit 982bea793ae14d91ff1af5fffd577dffbd85e335
    Author: Marco Baldi <60588169+marcobaldi97@users.noreply.github.com>
    Date:   Thu Sep 1 17:45:05 2022 -0300
    
        Adds example of excel upload with table of data (#4)
    
        * Adds example of excel upload with table of data

---
+++ src/pages/teachers/components/csv-uploader/CSVUploader.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useRef, useState } from "react";
+import * as XLSX from "xlsx";
+
+import { VisualComponent } from "../../../../core/interfaces";
+import { Card, Grid } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+interface Alumno {
+	ci: string;
+	apellidos: string;
+	nombre: string;
+}
+
+function CSVUploader(props: VisualComponent): React.ReactElement {
+	const { width, height } = props;
+
+	const [rows, setRows] = useState<Alumno[]>([]);
+	const [fileOver, setFileOver] = useState(false);
+
+	const fileInputRef = useRef(null);
+
+	useEffect(() => {
+		window.addEventListener(
+			"dragover",
+			function (e) {
+				e = e || event;
+				e.preventDefault();
+			},
+			false
+		);
+
+		window.addEventListener(
+			"drop",
+			function (e) {
+				e = e || event;
+				e.preventDefault();
+			},
+			false
+		);
+	});
+
+	const columns: GridColDef[] = [
+		{ field: "ci", headerName: "CI", width: 150 },
+		{ field: "apellidos", headerName: "Apellidos", width: 150 },
+		{ field: "nombres", headerName: "Nombres", width: 150 },
+	];
+
+	async function handleFileUpload(file: File): Promise<void> {
+		const data = await file.arrayBuffer();
+
+		const workbook = XLSX.read(data);
+
+		const rowsJson = XLSX.utils.sheet_to_json<Alumno>(workbook.Sheets[workbook.SheetNames[0]]);
+
+		setRows(rowsJson);
+	}
+
+	function handleOnClickUpload(event: React.ChangeEvent<HTMLInputElement>): void {
+		event.preventDefault();
+
+		const file = event.target.files?.[0];
+
+		if (!file) return;
+		else handleFileUpload(file);
+	}
+
+	function handleOnDropUpload(event: React.DragEvent<HTMLDivElement>): void {
+		event.preventDefault();
+		event.stopPropagation();
+
+		setFileOver(false);
+
+		const file = event.dataTransfer.files?.[0];
+
+		if (!file) return;
+		else handleFileUpload(file);
+	}
+
+	return (
+		<Grid width={width ?? 450} height={height ?? 400}>
+			<input type="file" ref={fileInputRef} onChange={handleOnClickUpload} style={{ display: "none" }} />
+
+			<DataGrid columns={columns} rows={rows} getRowId={(row): number => row.ci} pageSize={5} />
+
+			<Card
+				id="file-drop-zone"
+				onDrop={handleOnDropUpload}
+				onClick={(): void => {
+					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+					// @ts-ignore
+					fileInputRef.current.click();
+				}}
+				onDragEnter={(): void => setFileOver(true)}
+				onDragExit={(): void => setFileOver(false)}
+				style={{ background: fileOver ? "#cdcdcd" : "transparent" }}>
+				<Grid height={50}>
+					<h4>{fileOver ? "Tire el archivo aqui" : "Tire el archivo o presione aqui"}</h4>
+				</Grid>
+			</Card>
+		</Grid>
+	);
+}
+export default CSVUploader;
