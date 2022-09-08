5dcef09f8183a93f8db1ddd6fb272c9a88f519a2
Nacho <carmonaignacio@hotmail.com>
2022-09-08 14:37:15 -0300

    FE-7: added notifications icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,13 @@
-import { AppBar, Avatar, Box, Button, Menu, MenuItem, Toolbar } from "@mui/material";
+import { AppBar, Avatar, Badge, Box, Button, Chip, Menu, MenuItem, Toolbar } from "@mui/material";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DataStore } from "../../core/DataStore";
 import { UserRole } from "../../core/interfaces";
+import { userRoleToString } from "../../core/userRoleHelper";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 import "./Navbar.scss";
 
-function roleToAvatarColor(userRole: UserRole): string {
+function roleToAvatarColor(userRole?: UserRole): string {
 	switch (userRole) {
 		case UserRole.Administrador:
 			return "blue";
@@ -16,10 +18,10 @@ function roleToAvatarColor(userRole: UserRole): string {
 	}
 }
 
-function userNameToInitials(displayName: string): string {
+function userNameToInitials(displayName?: string): string {
 	return (
 		displayName
-			.match(/(\b\S)?/g)
+			?.match(/(\b\S)?/g)
 			?.join("")
 			.toUpperCase() ?? ":)"
 	);
@@ -27,25 +29,65 @@ function userNameToInitials(displayName: string): string {
 
 function Navbar(): React.ReactElement {
 	const navigate = useNavigate();
+
 	const dataStore = DataStore.getInstance();
 	const loggedUser = dataStore.loggedUser;
 
 	const [isOpenUserProfileMenu, setIsOpenUserProfileMenu] = useState(false);
-	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	const [anchorElProfile, setAnchorElProfile] = useState<null | HTMLElement>(null);
+	const [isOpenNotificationsMenu, setIsOpenNotificationsMenu] = useState(false);
+	const [anchorElNotifications, setAnchorElNotifications] = useState<null | HTMLElement>(null);
 
 	const handleProfileClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
 		setIsOpenUserProfileMenu(true);
-		setAnchorEl(event?.currentTarget);
+		setAnchorElProfile(event?.currentTarget);
+	};
+
+	const handleNotificationsClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
+		setIsOpenNotificationsMenu(true);
+		setAnchorElNotifications(event?.currentTarget);
 	};
 
 	const handleCloseProfileMenu = (): void => {
 		setIsOpenUserProfileMenu(false);
-		setAnchorEl(null);
+		setAnchorElProfile(null);
+	};
+
+	const handleCloseNotificationsMenu = (): void => {
+		setIsOpenNotificationsMenu(false);
+		setAnchorElNotifications(null);
 	};
 
 	const handleLogout = (): void => {
 		setIsOpenUserProfileMenu(false);
+
 		dataStore.logOut();
+	};
+
+	const renderUserAvatar = (): React.ReactElement => {
+		return <Avatar sx={{ bgcolor: roleToAvatarColor(loggedUser?.role) }}>{userNameToInitials(loggedUser?.displayName)}</Avatar>;
+	};
+
+	const renderProfileMenuContent = (): React.ReactElement => {
+		return (
+			<div className="menu-content">
+				<div className="user-info">
+					{renderUserAvatar()}
+					<div className="user-name-and-role">
+						{loggedUser?.displayName}
+						<Chip sx={{ bgcolor: roleToAvatarColor(loggedUser?.role) }} className="rolePill" label={userRoleToString(loggedUser?.role)} />
+					</div>
+				</div>
+
+				<MenuItem onClick={handleLogout} sx={{ justifyContent: "center" }}>
+					Cerrar Sesión
+				</MenuItem>
+			</div>
+		);
+	};
+
+	const renderNotificationseMenuContent = (): React.ReactElement => {
+		return <div>Nada por aquí aún</div>;
 	};
 
 	return (
@@ -54,12 +96,24 @@ function Navbar(): React.ReactElement {
 				<Toolbar sx={{ justifyContent: "flex-end", bgcolor: "gray" }}>
 					{loggedUser ? (
 						<div className="logged-user-controls">
-							<Button onClick={handleProfileClick}>
-								<Avatar sx={{ bgcolor: roleToAvatarColor(loggedUser.role) }}>{userNameToInitials(loggedUser.displayName)}</Avatar>
+							<Button onClick={handleNotificationsClick}>
+								<Badge badgeContent={3} color="secondary">
+									<NotificationsIcon className="notifications-icon" sx={{ color: "darkgray" }} />
+								</Badge>
 							</Button>
 
-							<Menu open={isOpenUserProfileMenu} onClose={handleCloseProfileMenu} anchorEl={anchorEl} className="profile-menu">
-								<MenuItem onClick={handleLogout}>Cerrar Sesión</MenuItem>
+							<Menu
+								open={isOpenNotificationsMenu}
+								onClose={handleCloseNotificationsMenu}
+								anchorEl={anchorElNotifications}
+								className="profile-menu">
+								{renderNotificationseMenuContent()}
+							</Menu>
+
+							<Button onClick={handleProfileClick}>{renderUserAvatar()}</Button>
+
+							<Menu open={isOpenUserProfileMenu} onClose={handleCloseProfileMenu} anchorEl={anchorElProfile} className="profile-menu">
+								{renderProfileMenuContent()}
 							</Menu>
 						</div>
 					) : (
