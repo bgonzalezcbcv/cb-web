c3583e573f02e876fea7dbc240c43ca0c1cc3374
Nacho <carmonaignacio@hotmail.com>
2022-09-17 11:47:17 -0300

    FE-5: refactor after PR comment

--- src/pages/studentForm/components/EnrollmentQuestions/EnrollmentQuestions.tsx
+++ src/pages/studentForm/components/EnrollmentQuestions/EnrollmentQuestions.tsx
@@ -1,7 +1,7 @@
 import _ from "lodash";
 import React, { useEffect, useState } from "react";
 
-import { Student } from "../../../../core/interfaces";
+import { Question, Student } from "../../../../core/interfaces";
 import { Box, Divider, Grid, List, ListItem, TextField, Typography } from "@mui/material";
 import useDebounce from "../../../../hooks/useDebounce";
 
@@ -9,6 +9,51 @@ export interface EnrollmentQuestionsProps {
 	studentData: Student;
 	editable: boolean;
 	onChange: (newData: Student) => void;
+}
+function renderQuestion(
+	onChangeQuestionCallback: (debouncedAnswerText: string) => void,
+	question: Question,
+	questionIndex: number,
+	editable: boolean
+): React.ReactElement {
+	const [answer, setAnswer] = useState(question.answer);
+	const debouncedAnswer = useDebounce<string>(answer, 500);
+
+	useEffect(() => {
+		onChangeQuestionCallback(debouncedAnswer);
+	}, [debouncedAnswer]);
+
+	return (
+		<Grid item sm={12} lg={6} xl={4} key={"question" + question.id + questionIndex} sx={{ width: "100%" }}>
+			<ListItem sx={{ flex: 1, height: "100%" }}>
+				<Box
+					sx={{
+						flexDirection: "column",
+						justifyContent: "space-between",
+						display: "flex",
+						flex: 1,
+						height: "100%",
+						alignContent: "center",
+					}}>
+					<Typography style={{ paddingRight: 20 }} gutterBottom variant="body2">
+						{question.question}
+					</Typography>
+					<TextField
+						multiline
+						minRows={4}
+						disabled={!editable}
+						maxRows={4}
+						fullWidth
+						placeholder="Ingrese la respuesta"
+						value={answer}
+						onChange={(event): void => {
+							setAnswer(event.target.value);
+						}}
+					/>
+				</Box>
+			</ListItem>
+		</Grid>
+	);
 }
 
 export function EnrollmentQuestions(props: EnrollmentQuestionsProps): React.ReactElement {
@@ -33,44 +78,10 @@ export function EnrollmentQuestions(props: EnrollmentQuestionsProps): React.Reac
 
 						<Grid container spacing={2} sx={{ justifyContent: "space-between" }}>
 							{category.questions.map((question, questionIndex): React.ReactElement => {
-								const [answer, setAnswer] = useState(question.answer);
-								const debouncedAnswer = useDebounce<string>(answer, 500);
-
-								useEffect(() => {
+								const onChangeQuestionHandler = (debouncedAnswer: string): void => {
 									onChangeHandler(categoryIndex, questionIndex, debouncedAnswer);
-								}, [debouncedAnswer]);
-
-								return (
-									<Grid item sm={12} lg={6} xl={4} key={"question" + question.id + questionIndex} sx={{ width: "100%" }}>
-										<ListItem sx={{ flex: 1, height: "100%" }}>
-											<Box
-												sx={{
-													flexDirection: "column",
-													justifyContent: "space-between",
-													display: "flex",
-													flex: 1,
-													height: "100%",
-													alignContent: "center",
-												}}>
-												<Typography style={{ paddingRight: 20 }} gutterBottom variant="body2">
-													{question.question}
-												</Typography>
-												<TextField
-													multiline
-													minRows={4}
-													disabled={!editable}
-													maxRows={4}
-													fullWidth
-													placeholder="Ingrese la respuesta"
-													value={answer}
-													onChange={(event): void => {
-														setAnswer(event.target.value);
-													}}
-												/>
-											</Box>
-										</ListItem>
-									</Grid>
-								);
+								};
+								return renderQuestion(onChangeQuestionHandler, question, questionIndex, editable);
 							})}
 						</Grid>
 
