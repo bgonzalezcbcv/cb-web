1af5afc06593e52aad1c95594939effd0eb8008d
Nacho <carmonaignacio@hotmail.com>
2022-09-17 15:18:21 -0300

    Comentarios del PR
    Refactor student
    
    Mejorando colores y agregando botones
    
    Mejorando colores y agregando botones
    
    Mejorando colores y agregando botones
    
    Mejorando colores y agregando botones
    
    Mejorando colores y agregando botones
    
    Mejorando colores y agregando botones
    
    FE-9: add ThemeProvider and theme

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,52 +1,60 @@
-import React , {useCallback, useEffect, useState} from "react";
-import {Box, Button, Modal as MUIModal} from "@mui/material";
+import React, { useCallback, useEffect, useState } from "react";
+
+import { Box, Button, IconButton, Modal as MUIModal, Typography } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+
 import "./Modal.scss";
 
 export type ModalProps = {
-    show: boolean;
-    title: string;
-    body: React.ReactElement;
-    cancelText?: string;
-    acceptText?: string;
-    onClose: () => void;
-    onAccept: () => void;
-}
+	show: boolean;
+	title: string;
+	body: React.ReactElement;
+	cancelText?: string;
+	acceptText?: string;
+	onClose: () => void;
+	onAccept: () => void;
+};
 
 export default function Modal(props: ModalProps): React.ReactElement {
-    const {acceptText, body, cancelText, show, title, onClose, onAccept} = props;
+	const { acceptText, body, cancelText, show, title, onClose, onAccept } = props;
 
-    const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    useEffect( () => {
-        setIsOpen(show);
-    }, [show]);
+	useEffect(() => {
+		setIsOpen(show);
+	}, [show]);
 
-    const handleModalClose = useCallback(() => {
-        setIsOpen(false);
-        onClose();
-    }, [onClose]);
+	const handleModalClose = useCallback(() => {
+		setIsOpen(false);
+		onClose();
+	}, [onClose]);
 
-    const handleModalAccept = useCallback(() => {
-        setIsOpen(false);
-        onAccept();
-    }, [onAccept]);
+	const handleModalAccept = useCallback(() => {
+		setIsOpen(false);
+		onAccept();
+	}, [onAccept]);
 
-    return (
-        <MUIModal open={isOpen} onClose={handleModalClose}>
-            <Box className="modal">
-                <CloseIcon onClick={handleModalClose} />
+	return (
+		<MUIModal open={isOpen} onClose={handleModalClose}>
+			<Box className="modal">
+				<IconButton>
+					<CloseIcon onClick={handleModalClose} />
+				</IconButton>
 
-                <div className="wrapper">
-                    <h3>{title}</h3>
-                    {body}
-                </div>
+				<div className="wrapper">
+					<Typography variant={"h6"}>{title}</Typography>
+					{body}
+				</div>
 
-                <div className="button-container">
-                    <Button variant="outlined" sx={{marginRight: 2}} onClick={handleModalClose}>{cancelText ?? "Cancelar"}</Button>
-                    <Button variant="contained" onClick={handleModalAccept}>{acceptText ?? "Aceptar"}</Button>
-                </div>
-            </Box>
-        </MUIModal>
-    );
+				<div className="button-container">
+					<Button variant="outlined" color={"secondary"} sx={{ marginRight: 2 }} onClick={handleModalClose}>
+						{cancelText ?? "Cancelar"}
+					</Button>
+					<Button variant="contained" color={"secondary"} onClick={handleModalAccept}>
+						{acceptText ?? "Aceptar"}
+					</Button>
+				</div>
+			</Box>
+		</MUIModal>
+	);
 }
