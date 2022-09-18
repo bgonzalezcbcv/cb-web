f6cd74941686d639a1cb8cbfa6c9f56b8618a726
Eva <eva@pomelogames.com>
2022-09-18 15:07:10 -0300

    Discount section working

--- src/pages/student/components/AdministrativeInfo/DiscountsSection/DiscountSection.tsx
+++ src/pages/student/components/AdministrativeInfo/DiscountsSection/DiscountSection.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable */
 import React, { useCallback, useState } from "react";
 
-import { createAjv } from "@jsonforms/core";
 import * as Models from "../../../../../core/Models";
 import { VisualComponent } from "../../../../../core/interfaces";
-import FileUploader from "../../../../../components/fileUploader/FileUploader";
 import Modal from "../../../../../components/modal/Modal";
 import DiscountHistory from ".././historyTables/DiscountHistory";
-import { Card, CardContent, Divider, FormControl, InputAdornment, InputLabel, MenuItem, Select, TextField, TextFieldProps } from "@mui/material";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Card, CardContent, Divider } from "@mui/material";
+import { JsonForms } from "@jsonforms/react";
+import { JsonSchema7, Translator, createAjv } from "@jsonforms/core";
+import { materialCells, materialRenderers } from "@jsonforms/material-renderers";
 
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-import DownloadIcon from "@mui/icons-material/Download";
+
+import schema from "../../../schema.json";
+import ui from "./ui.json";
 
 import "./DiscountSection.scss";
 
@@ -22,19 +23,23 @@ export type AdministrativeInfoProps = {
 	onChange: (data: Models.Student) => void;
 };
 
+type DiscountData = {
+	percentage: number;
+	starting_date: string;
+	ending_date: string;
+	type: string;
+	resolution_url: string;
+	explanation: string;
+	report_url: string;
+	description: string;
+};
+
 export default function DiscountSection(props: VisualComponent & AdministrativeInfoProps): React.ReactElement {
 	const { editable, student, onChange } = props;
 
 	const [discountModalOpen, setDiscountModalOpen] = useState(false);
-	const [discountPercentage, setDiscountPercentage] = useState("");
-	const [discountReason, setDiscountReason] = useState("");
-	const [discountType, setDiscountType] = useState("");
-	const [discountIniDate, setDiscountIniDate] = useState<Date>(new Date());
-	const [discountFinDate, setDiscountFinDate] = useState<Date>(new Date());
-	const [resolutionDocument, setResolutionDocument] = useState<File>();
-	const [reportDocument, setReportDocument] = useState<File>();
 
-	const [discountDescription, setDiscountDescription] = useState("");
+	const [discountData, setDiscountData] = useState<DiscountData>({} as DiscountData);
 
 	const handleDiscountModalOpen = useCallback(() => {
 		setDiscountModalOpen(true);
@@ -45,26 +50,26 @@ export default function DiscountSection(props: VisualComponent & AdministrativeI
 	}, []);
 
 	//TODO: Adjust this when file handling is defined
-	const handleAddNewDiscount = useCallback(() => {
+	const handleAddNewDiscount = useCallback((discountData: DiscountData) => {
 		const newDiscount: Models.Discount = {
-			percentage: Number(discountPercentage),
-			starting_date: discountIniDate,
-			ending_date: discountFinDate,
-			type: discountType as Models.DiscountType,
-			resolution_url: resolutionDocument?.name ?? "",
-			explanation: discountReason as Models.DiscountExplanation,
-			report_url: reportDocument?.name ?? "",
-			description: discountDescription,
+			percentage: discountData.percentage,
+			starting_date: new Date(discountData.starting_date),
+			ending_date: new Date(discountData.ending_date),
+			type: discountData.type as Models.DiscountType,
+			resolution_url: discountData.resolution_url,
+			explanation: discountData.explanation as Models.DiscountExplanation,
+			report_url: discountData.report_url,
+			description: discountData.description,
 		};
 
 		const updatedStudent = {
 			...student,
-			administrative_info: { ...student.administrative_info, discount: [newDiscount, ...student.administrative_info.discounts] },
+			administrative_info: { ...student.administrative_info, discounts: [newDiscount, ...student.administrative_info.discounts] },
 		};
 		onChange(updatedStudent);
+		handleDiscountModalClose();
 	}, []);
 
-	// @ts-ignore
 	return (
 		<Card className="discount-wrapper">
 			<CardContent className="payment-content">
@@ -78,104 +83,29 @@ export default function DiscountSection(props: VisualComponent & AdministrativeI
 							show={discountModalOpen}
 							title={"Agregar un nuevo descuento"}
 							body={
-								<div className="discount-container-wrapper">
-									<div className="validity-dates-container">
-										<LocalizationProvider dateAdapter={AdapterDayjs}>
-											<DatePicker
-												label="Inicio"
-												value={discountIniDate}
-												inputFormat={"D/MM/YYYY"}
-												onChange={(newValue) => {
-													return setDiscountIniDate(newValue ?? new Date());
-												}}
-												renderInput={(params: JSX.IntrinsicAttributes & TextFieldProps) => (
-													<TextField {...params} variant={"standard"} />
-												)}
-											/>
-										</LocalizationProvider>
-
-										<LocalizationProvider dateAdapter={AdapterDayjs}>
-											<DatePicker
-												label="Fin"
-												value={discountFinDate}
-												inputFormat={"D/MM/YYYY"}
-												onChange={(newValue) => {
-													return setDiscountFinDate(newValue ?? new Date());
-												}}
-												renderInput={(params: JSX.IntrinsicAttributes & TextFieldProps) => (
-													<TextField {...params} variant={"standard"} />
-												)}
-											/>
-										</LocalizationProvider>
-									</div>
-
-									<div className="discount-container">
-										<TextField
-											label={"Porcentaje"}
-											value={discountPercentage}
-											variant="standard"
-											sx={{ width: 100 }}
-											InputProps={{ endAdornment: <InputAdornment position={"end"}>%</InputAdornment> }}
-											onChange={(event) => setDiscountPercentage(event.target.value)}
-										/>
-
-										<FormControl variant="standard" sx={{ width: 150 }}>
-											<InputLabel id="discount-reason-label">Explicación</InputLabel>
-
-											<Select
-												labelId="discount-reason"
-												id="discount-reason"
-												label="Explicación del descuento"
-												value={discountReason}
-												onChange={(event) => {
-													const newStudent = {
-														...student,
-														administrative_info: { ...student.administrative_info },
-													};
-													onChange(newStudent);
-
-													setDiscountReason(event.target.value);
-												}}>
-												<MenuItem value={"hermano"}>Hermano</MenuItem>
-												<MenuItem value={"resolucion"}>Resolución</MenuItem>
-											</Select>
-										</FormControl>
-
-										<TextField
-											label={"Descripción"}
-											value={discountDescription}
-											variant="standard"
-											onChange={(event) => {
-												setDiscountDescription(event.target.value);
-											}}
-										/>
-									</div>
-
-									<div className="resolution-document">
-										<FileUploader label={"Adjunto resolución"} width={200} uploadedFile={(file) => setResolutionDocument(file)} />
-									</div>
-
-									<div className="discount-type-container">
-										<FormControl variant="standard" sx={{ width: 150 }}>
-											<InputLabel id="discount-type-label">Tipo</InputLabel>
-
-											<Select
-												labelId="discount-type"
-												id="discount-type"
-												label="Tipo"
-												value={discountType}
-												onChange={(event) => setDiscountType(event.target.value)}>
-												<MenuItem value={"direccion"}>Dirección</MenuItem>
-												<MenuItem value={"asistente_social"}>Asistente social</MenuItem>
-											</Select>
-										</FormControl>
-
-										<FileUploader label={"Adjunto informe"} width={200} uploadedFile={(file) => setReportDocument(file)} />
-									</div>
-								</div>
+								<JsonForms
+									schema={schema as JsonSchema7}
+									uischema={ui}
+									data={{ administrative_info: { discounts: [discountData] } }}
+									renderers={materialRenderers}
+									cells={materialCells}
+									onChange={({ data, errors }): void => {
+										if (
+											data &&
+											data.administrative_info &&
+											data.administrative_info.discounts &&
+											data.administrative_info.discounts.length > 0
+										) {
+											const info = data.administrative_info.discounts[0];
+											setDiscountData(info);
+										}
+									}}
+								/>
 							}
 							onClose={handleDiscountModalClose}
-							onAccept={handleAddNewDiscount}
+							onAccept={() => {
+								handleAddNewDiscount(discountData);
+							}}
 						/>
 					</div>
 				</div>
