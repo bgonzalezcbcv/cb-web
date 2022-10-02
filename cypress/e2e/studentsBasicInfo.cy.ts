1fea7e5489aa1e65def3029cbd240be0e6acdbc4
Chuardo <egastelu@gmail.com>
2022-10-02 18:41:16 -0300

    wip

--- cypress/e2e/studentsBasicInfo.cy.ts
+++ cypress/e2e/studentsBasicInfo.cy.ts
@@ -1,6 +1,6 @@
 describe("studentsBasicInfo",() => {
     const basicInfoButtonID = ".MuiTabs-flexContainer > :nth-child(1)"
-    const editButtonID= ".css-zi9jgv > :nth-child(2) > :nth-child(2)"
+    const editButtonID= '[data-cy="studentEditInfoButton"]'
     const nameFieldID = "#\\#\\/properties\\/name2-input";
     const surnameFieldID = "#\\#\\/properties\\/surname2-input";
     const CIFieldID = "#\\#\\/properties\\/ci2-input";
@@ -31,23 +31,41 @@ describe("studentsBasicInfo",() => {
         cy.get(basicInfoButtonID).click();
         cy.get(editButtonID).click();
         cy.get(nameFieldID).clear().type("Adam");
+        cy.wait(200);
         cy.get(surnameFieldID).clear().type("Sandler");
+        cy.wait(200);
         cy.get(CIFieldID).clear().type("11111111");
+        cy.wait(200);
         cy.get(statusFieldID).type("Pendiente");
+        cy.wait(200);
         cy.get(tuitionFieldID).type("Matricula buena");
+        cy.wait(200);
         cy.get(GroupFieldID).type("3");
+        cy.wait(200);
         cy.get(SubGroupFieldID).type("A");
+        cy.wait(200);
         cy.get(referenceNumberFieldID).type("42");
+        cy.wait(200);
         cy.get(placeOfBirthFieldID).type("Asociacion Española");
-        cy.get(dateOfBirthInputID).type("2000-09-01");
+        cy.wait(200);
+        cy.get(dateOfBirthInputID).type("01/09/2000");
+        cy.wait(200);
         cy.get(nationalityFieldID).type("Uruguayo");
+        cy.wait(200);
         cy.get(firstLanguageFieldID).type("Español");
+        cy.wait(200);
         cy.get(neighborhoodFieldID).type("Cordon");
+        cy.wait(200);
         cy.get(addressFieldID).type("Guayabo 1729");
+        cy.wait(200);
         cy.get(medicalAssuranceFieldID).type("Si tiene es la A23BVJ");
+        cy.wait(200);
         cy.get(emergencyFieldID).type("Cooperativa ABC123XYZ");
+        cy.wait(200);
         cy.get(vaccineExpirationInputID).type("03/03/2019");
+        cy.wait(200);
         cy.fillStudentFamilyInfo();
+        cy.wait(200);
         cy.get(createButtonID).click();
 
         cy.get("body").should("not.include.text","Hay errores en los campos del alumno")
