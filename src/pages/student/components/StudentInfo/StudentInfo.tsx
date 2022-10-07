a921611dc2ff864c4bb5fac557b287adfb62e24f
Nadia Martinez <nmartinezpetrone@gmail.com>
2022-10-06 23:15:50 -0300

    FE-18: Fix required fields

--- src/pages/student/components/StudentInfo/StudentInfo.tsx
+++ src/pages/student/components/StudentInfo/StudentInfo.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { JsonForms } from "@jsonforms/react";
 import { materialCells, materialRenderers } from "@jsonforms/material-renderers";
+import {Translator} from "@jsonforms/core";
 import { Student } from "../../../../core/Models";
 
 import { ajv as studentAjv } from "../../../../core/AJVHelper";
@@ -20,9 +21,15 @@ export type StudentInfoProps = {
 export default function StudentInfo(props: StudentInfoProps): React.ReactElement {
 	const { editable, student, onChange } = props;
 
+	const translator = (id: string, defaultMessage: string): string => {
+		if (id.includes("required")) return "Campo requerido";
+		else return defaultMessage;
+	};
+
 	return (
 		<div style={{ paddingTop: "30px" }}>
 			<JsonForms
+				i18n={{ translate: translator as Translator }}
 				ajv={studentAjv}
 				schema={schema}
 				uischema={uischema}
