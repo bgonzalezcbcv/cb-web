43c197ba0087115236ddb13ea975c88f37d5d9de
Santiago Segundo <santiago.segundo@kzsoftworks.com>
2022-10-11 21:59:47 -0300

    add NumericInput component

---
+++ src/components/NumericInput/NumericInput.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { TextField } from "@mui/material";
+
+export type NumericInputProps = {
+	isFloat: boolean;
+	maxLength?: number;
+};
+
+export function NumericInput(props: NumericInputProps): React.ReactElement {
+	const { isFloat, maxLength } = props;
+
+	const [numberValue, setNumberValue] = React.useState("");
+
+	const handeValueChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+		let numberRegex;
+		let value;
+		if (isFloat) {
+			numberRegex = /^(0|[1-9]*)(\.[0-9]{0,2})?$/;
+			value = parseFloat(event.target.value);
+		} else {
+			numberRegex = /^[1-9][0-9]*$/;
+			value = parseInt(event.target.value);
+		}
+
+		const testInt = numberRegex.test(event.target.value);
+
+		if (event.target.value === "" || (testInt && value >= 0)) setNumberValue(event.target.value);
+	};
+
+	const handleOnBlur = (event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+		const value = event.target.value;
+		const numberValue = parseFloat(value);
+		const trailingZeroesRegex = /^(0|[1-9]*)(\.(00|[1-9]0))?$/;
+
+		if (numberValue === 0) setNumberValue("");
+		else if (trailingZeroesRegex.test(value)) {
+			const result = value.replace(/.00$|0$/, "");
+			setNumberValue(result);
+		}
+	};
+
+	return (
+		<TextField
+			inputProps={{ maxLength: maxLength }}
+			variant="standard"
+			value={numberValue}
+			onChange={(event) => handeValueChange(event)}
+			onBlur={(event) => {
+				if (isFloat) handleOnBlur(event);
+			}}
+		/>
+	);
+}
