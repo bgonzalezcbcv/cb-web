d9e1d881054de83cc6eff90b4309e64ab50e86b6
Santiago Segundo <santiago.segundo@kzsoftworks.com>
2022-10-22 16:19:12 -0300

    wip

--- src/pages/reportcard/ReportCardList.tsx
+++ src/pages/reportcard/ReportCardList.tsx
@@ -2,17 +2,52 @@ import _ from "lodash";
 import React, { useCallback, useEffect, useState } from "react";
 
 import * as API from "../../core/ApiStore";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { DataGrid, GridCellValue, GridColDef } from "@mui/x-data-grid";
+import CloseIcon from "@mui/icons-material/Close";
+import RemoveIcon from "@mui/icons-material/Remove";
 import { ReportCard, ReportCard as ReportCardModel } from "../../core/Models";
-import { Alert, Box, Card, CircularProgress, Input, Paper, Typography } from "@mui/material";
+import { Alert, Box, Card, CircularProgress, Icon, Input, Paper, Typography } from "@mui/material";
 import { FetchState } from "../../core/interfaces";
+
+// starting_month: string;
+// ending_month: string;
 
 const columns: GridColDef[] = [
 	{ field: "id", headerName: "ID", disableColumnMenu: false, flex: 1 },
 	{ field: "grade", headerName: "Grado", disableColumnMenu: false, flex: 2 },
-	{ field: "period", headerName: "Periodo", disableColumnMenu: false, flex: 2 },
+	{
+		field: "",
+		headerName: "Periodo",
+		disableColumnMenu: false,
+		flex: 2,
+		renderCell: (params): React.ReactElement => {
+			let period = "";
+
+			params.row.type === "Intermedio"
+				? (period = `${params.row.starting_month} - ${params.row.ending_month}`)
+				: (period = `${params.row.starting_month}`);
+
+			return <span>{period}</span>;
+		},
+	},
 	{ field: "type", headerName: "Tipo", disableColumnMenu: false, flex: 2 },
-	{ field: "passed", headerName: "Aprobado", disableColumnMenu: false, flex: 2 },
+	{
+		field: "passed",
+		headerName: "Aprobado",
+		disableColumnMenu: false,
+		flex: 1,
+		renderCell: (params): React.ReactElement => {
+			let cellValue = "N/A";
+			const isFinal = params.row.type === "Final";
+			const passed = params.row.passed === true;
+
+			if (isFinal) {
+				passed ? (cellValue = "SI") : (cellValue = "NO");
+			}
+
+			return <span>{cellValue}</span>;
+		},
+	},
 ];
 
 export const emptyReport: ReportCard = {
@@ -68,7 +103,7 @@ export default function ReportCardList(props: ReportCardListProps): React.ReactE
 				return (
 					<Box style={{ padding: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
 						<Alert severity="error" variant="outlined">
-							Falló la carga de alumnos.
+							Falló la carga de boletines.
 						</Alert>
 					</Box>
 				);
