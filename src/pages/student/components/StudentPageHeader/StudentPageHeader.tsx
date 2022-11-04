78f87c1c86a822bfe536aeb3297b558998e807a7
Nadia Martinez <nmartinezpetrone@gmail.com>
2022-11-03 23:01:57 -0300

    FE-216: Agregado modal para dar de baja alumno

--- src/pages/student/components/StudentPageHeader/StudentPageHeader.tsx
+++ src/pages/student/components/StudentPageHeader/StudentPageHeader.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { Student } from "../../../../core/Models";
 import { StudentPageMode } from "../../../../core/interfaces";
 
-import { Box, Button, Chip, Typography } from "@mui/material";
+import {Box, Button, Chip, FormControl, InputLabel, MenuItem, Select, TextField, Typography} from "@mui/material";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
 import EditIcon from "@mui/icons-material/Edit";
 import EditOffIcon from "@mui/icons-material/EditOff";
@@ -15,6 +15,14 @@ import PriorityHighIcon from "@mui/icons-material/PriorityHigh";
 import FormUploadDialog from "../FormUploadDialog/FormUploadDialog";
 
 import "./Student.scss";
+import Modal from "../../../../components/modal/Modal";
+import DatePickerToString from "../../../../components/datePicker/DatePicker";
+
+type DeactivationInfo = {
+	lastDay: string;
+	reason: string;
+	description: string;
+};
 
 interface StudentPageHeaderProps {
 	mode: StudentPageMode;
@@ -28,6 +36,8 @@ export default function StudentPageHeader(props: StudentPageHeaderProps): React.
 	const { mode, setStudent, setIsEditable, isEditable, student } = props;
 
 	const [isFormUploadOpen, setIsFormUploadOpen] = React.useState(false);
+	const [isDeactivateStudentModalVisible, setIsDeactivateStudentModalVisible] = React.useState(false);
+	const [deactivationInfo, setDeactivationInfo] = React.useState<DeactivationInfo>({} as DeactivationInfo);
 
 	return (
 		<>
@@ -87,8 +97,8 @@ export default function StudentPageHeader(props: StudentPageHeaderProps): React.
 					) : null}
 
 					{mode === StudentPageMode.edit ? (
-						<Button color={"secondary"} startIcon={<DeleteIcon />}>
-							Bajar
+						<Button color={"secondary"} startIcon={<DeleteIcon />} onClick={() => setIsDeactivateStudentModalVisible(true)}>
+							Dar de baja
 						</Button>
 					) : (
 						""
@@ -105,6 +115,56 @@ export default function StudentPageHeader(props: StudentPageHeaderProps): React.
 					setIsFormUploadOpen(false);
 				}}
 			/>
+
+			<Modal
+				show={isDeactivateStudentModalVisible}
+				title={"Dar de baja estudiante"}
+				acceptText={"Dar de baja"}
+				onClose={() => setIsDeactivateStudentModalVisible(false)}
+				onAccept={() => {}}
+				acceptEnabled={true}
+			>
+				<Box>
+					<DatePickerToString
+						date={deactivationInfo.lastDay}
+						label={"Último día"}
+						editable={true}
+						required={true}
+						onChange={(date: string): void => {
+						const newInfo = { ...deactivationInfo, lastDay: date };
+						setDeactivationInfo(newInfo);
+					}} />
+
+					<FormControl variant="standard" sx={{width: "100%", marginTop: "5px"}} required={true}>
+						<InputLabel id="reason-label">Motivo</InputLabel>
+
+						<Select
+							labelId="reason-label"
+							id="reason-select"
+							value={deactivationInfo.reason}
+							label="Motivo"
+							onChange={(event): void => {
+								const newInfo = { ...deactivationInfo, reason: event.target.value };
+								setDeactivationInfo(newInfo);
+							}}
+						>
+							<MenuItem value={10}>Motivo 1</MenuItem>
+							<MenuItem value={20}>Motivo 2</MenuItem>
+							<MenuItem value={30}>Motivo 3</MenuItem>
+						</Select>
+
+						<TextField
+							id="standard-multiline-static"
+							sx={{marginTop: "5px"}}
+							label="Descripción"
+							multiline
+							rows={4}
+							variant="standard"
+							required={true}
+						/>
+					</FormControl>
+				</Box>
+			</Modal>
 		</>
 	);
 }
