c37118dce402a5c999b607e868fe4739e07356fe
Santiago <37115443+santiagosegundo96@users.noreply.github.com>
2022-11-14 21:31:22 -0300

    BUG-322 fix (#125)
    Co-authored-by: Santiago Segundo <santiago.segundo@kzsoftworks.com>

--- src/hooks/useFetchFromAPI.tsx
+++ src/hooks/useFetchFromAPI.tsx
@@ -13,11 +13,13 @@ export enum FetchStatus {
  * pass this parameter as () => myAPIFunc(id)
  * @param onFetch: this is the setter that will be used when the fetch was correct to save the data.
  * @param shouldFetch: Default to true and it could be used to disallow the fetching (conditional fetching).
+ * @param dependencyArray
  */
 function useFetchFromAPI<Type>(
 	fetchFunction: (...params: unknown[]) => Promise<DefaultApiResponse<Type>>,
 	onFetch: (data: Type) => void,
-	shouldFetch = true
+	shouldFetch = true,
+	dependencyArray: unknown[] = []
 ): { fetchStatus: FetchStatus; refetch: () => void } {
 	const [fetchStatus, setFetchStatus] = React.useState(FetchStatus.Fetching);
 
@@ -30,11 +32,11 @@ function useFetchFromAPI<Type>(
 			onFetch(response.data);
 			setFetchStatus(FetchStatus.Initial);
 		} else setFetchStatus(FetchStatus.Error);
-	}, []);
+	}, dependencyArray);
 
 	useEffect(() => {
 		shouldFetch && fetchHandler();
-	}, []);
+	}, dependencyArray);
 
 	return {
 		fetchStatus: shouldFetch ? fetchStatus : FetchStatus.Initial,
