e3d1d5981b4d2c17292c7435c0198841b3b3e98c
marcobaldi97 <60588169+marcobaldi97@users.noreply.github.com>
2022-11-29 17:13:28 -0300

    Pending and inactive students list fix.

--- src/pages/students/inactive/InactiveStudents.tsx
+++ src/pages/students/inactive/InactiveStudents.tsx
@@ -1,159 +1,24 @@
-/*eslint-disable*/
-import _ from "lodash";
-import React, { useCallback, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
-import { DataGrid, GridApi, GridCellValue, GridColDef } from "@mui/x-data-grid";
-import { Alert, Box, Button, Card, CircularProgress, Input, Paper, TextField, Typography } from "@mui/material";
-import * as APIStore from "../../../core/ApiStore";
-import { FetchState } from "../../../core/interfaces";
-import { normalizeText, apiDateToDisplayDate } from "../../../core/CoreHelper";
-import { InactiveStudentInfo as StudentModel } from "../../../core/Models";
-import { emptyStudents } from "../../student/DefaultStudent";
+import { Alert, CircularProgress } from "@mui/material";
 
-import "../Students.scss";
-const columns: GridColDef[] = [
-	{ field: "reference_number", headerName: "Nº de referencia", disableColumnMenu: false, flex: 1 },
-	{ field: "ci", headerName: "CI", disableColumnMenu: false, flex: 1 },
-	{ field: "name", headerName: "Nombres", disableColumnMenu: false, flex: 2 },
-	{ field: "surname", headerName: "Apellidos", disableColumnMenu: false, flex: 2 },
-	{
-		field: "date",
-		headerName: "Fecha de baja",
-		disableColumnMenu: false,
-		flex: 1,
-		valueGetter: (params) =>
-			params.row.last_motive_inactivate
-				? params.row.last_motive_inactivate.last_day
-					? apiDateToDisplayDate(params.row.last_motive_inactivate.last_day)
-					: ""
-				: "",
-	},
-	{
-		field: "motive",
-		headerName: "Motivo",
-		disableColumnMenu: false,
-		flex: 2,
-		valueGetter: (params) => (params.row.last_motive_inactivate ? params.row.last_motive_inactivate.motive : ""),
-	},
-	{
-		field: "description",
-		headerName: "Descripción",
-		disableColumnMenu: false,
-		flex: 2,
-		valueGetter: (params) => (params.row.last_motive_inactivate ? params.row.last_motive_inactivate.description : ""),
-	},
-	{
-		field: "id",
-		headerName: "Ir al alumno",
-		align: "center",
-		sortable: false,
-		disableColumnMenu: true,
-		flex: 1,
-		renderCell: (params) => {
-			const navigate = useNavigate();
+import { Student } from "../../../core/Models";
+import { fetchInactiveStudents } from "../../../core/ApiStore";
+import Students from "../Students";
+import useFetchFromAPI, { FetchStatus } from "../../../hooks/useFetchFromAPI";
 
-			const onClick = (e: any) => {
-				e.stopPropagation();
+export default function InactiveStudents(): JSX.Element {
+	const [students, setStudents] = useState<Student[]>();
+	const { fetchStatus, refetch } = useFetchFromAPI(() => fetchInactiveStudents(), setStudents);
 
-				navigate("/student/" + params.value + "/edit");
-			};
-			return <Button onClick={onClick}>Ir</Button>; //Link component
-		},
-	},
-];
+	if (fetchStatus === FetchStatus.Fetching) return <CircularProgress />;
 
-export default function InactiveStudents() {
-	const [students, setStudents] = useState<StudentModel[]>([]);
-	const [fetchState, setFetchState] = React.useState(FetchState.initial);
-	const [searchText, setSearchText] = React.useState("");
+	if (fetchStatus === FetchStatus.Error)
+		return (
+			<Alert severity="error" variant="outlined" onClick={refetch} style={{ cursor: "pointer" }}>
+				No se pudieron obtener los alumnos inactivos. Haga click aquí para reintentar.
+			</Alert>
+		);
 
-	const getStudents = useCallback(async (): Promise<void> => {
-		setFetchState(FetchState.loading);
-
-		const response = await APIStore.fetchInactiveStudents();
-
-		if (response.success && response.data) {
-			setStudents(response.data);
-			setFetchState(FetchState.initial);
-		} else setFetchState(FetchState.failure);
-	}, [setFetchState, setStudents]);
-
-	useEffect((): void => {
-		getStudents();
-	}, []);
-
-	const printTable = useCallback((): JSX.Element | null => {
-		switch (fetchState) {
-			case "loading":
-				return (
-					<Box style={{ padding: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-						<CircularProgress />
-					</Box>
-				);
-			case "failure":
-				return (
-					<Box style={{ padding: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-						<Alert severity="error" variant="outlined">
-							Falló la carga de alumnos.
-						</Alert>
-					</Box>
-				);
-			case "initial":
-				const foundItems = students.filter(
-					(student) =>
-						Object.values(student).some((value) => value && normalizeText(value.toString()).includes(normalizeText(searchText))) ||
-						Object.values(student.last_motive_inactivate).some(
-							(value) => value && normalizeText(value.toString()).includes(normalizeText(searchText))
-						)
-				);
-
-				return (
-					<DataGrid //
-						style={{ height: 380, width: "100%" }}
-						rows={foundItems}
-						columns={columns}
-						pageSize={5}
-						rowsPerPageOptions={[5]}
-						initialState={{
-							sorting: {
-								sortModel: [{ field: "date", sort: "asc" }],
-							},
-						}}
-					/>
-				);
-			default:
-				return null;
-		}
-	}, [fetchState, students, searchText]);
-
-	return (
-		<Card
-			sx={{
-				width: "90%",
-				padding: "20px",
-				alignSelf: "flex-start",
-				marginTop: "20px",
-				display: "flex",
-				flexDirection: "column",
-				boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
-			}}>
-			<Box display="flex" justifyContent="flex-start" width="100%">
-				<Typography variant="h4">Alumnos Inactivos</Typography>
-			</Box>
-
-			<Box className="SearchAndGroupFilter">
-				<Input
-					style={{ flex: 3, marginRight: "10%" }}
-					id="search"
-					type="text"
-					placeholder="Buscar..."
-					value={searchText}
-					onChange={(e: React.ChangeEvent<any>) => setSearchText(e.target.value)}
-				/>
-			</Box>
-
-			<Paper>{printTable()}</Paper>
-		</Card>
-	);
+	return <Students title="Alumnos inactivos" rows={students} />;
 }
